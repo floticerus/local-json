27eec2afadd5aee49b50a296753a400adf086d5c
kevin von flotow <vonflow@gmail.com>
2014-11-15 07:39:03 -0600

    add file cache, watch with chokidar

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 /*
-  local-json v0.0.4
+  local-json v0.0.5
   copyright 2014 - kevin von flotow
   MIT license
 */
@@ -13,21 +13,95 @@
 
         var deepExtend = require( 'deep-extend' )
 
+        var chokidar = require( 'chokidar' )
+
+        var mainQueue = new Queue( 5 )
+
+        var fileData = {}
+
         function noop(){}
 
-        function readData( fn, file )
+        // pass the full path of the file
+        function watchFile( file )
+        {
+            // make sure file is at least something
+            if ( !file || '' === file )
+            {
+                return
+            }
+
+            // make sure it's a string
+            file = file.toString()
+
+            // watch it
+            var watcher = chokidar.watch( file, { ignored:  /[\/\\]\./, persistent: true } )
+
+            var that = this
+
+            // setup event listeners
+            watcher
+                // listener for when the file has been removed
+                .on( 'unlink', function ( path )
+                    {
+                        // delete cached file data if it exists
+                        if ( fileData.hasOwnProperty( path ) )
+                        {
+                            delete fileData[ path ]
+                        }
+
+                        // remove the watcher
+                        watcher.close()
+                    }
+                )
+
+                // listener for when the file has been changed
+                .on( 'change', function ( path )
+                    {
+                        // add to main file processing queue
+                        mainQueue.add( function ( mainDone )
+                            {
+                                // attempt to read the file
+                                fs.readFile( path, function ( err, fileContents )
+                                    {
+                                        // notify nbqueue that the async function has finished,
+                                        // regardless of success
+                                        mainDone()
+
+                                        // check for errors
+                                        if ( err )
+                                        {
+                                            // see if LocalJson instance logging is enabled
+                                            if ( that.opts.logging )
+                                            {
+                                                console.log( err )
+                                            }
+
+                                            return 
+                                        }
+
+                                        // cache new json
+                                        fileData[ path ] = parseData.call( that, JSON.parse, fileContents )
+                                    }
+                                )
+                            }
+                        )
+                    }
+                )
+        }
+
+        function parseData( fn, fileContents )
         {
             var add = {}
 
             // don't crash the server if the json is invalid
             try
             {
-                add = fn( file )
+                add = fn( fileContents )
             }
 
             catch ( e )
             {
-                if ( this.logging )
+                if ( this.opts.logging )
                 {
                     // bad json or not found
                     console.log( 'json error', e )
@@ -84,14 +158,14 @@
 
                 if ( !this.opts.dynamic )
                 {
-                    file.push( readData.call( this, require, filePath ) )
+                    file.push( parseData.call( this, require, filePath ) )
 
                     continue
                 }
 
                 var data = fs.readFileSync( filePath, { encoding: 'utf8' } )
 
-                files.push( readData.call( this, JSON.parse, data ) )
+                files.push( parseData.call( this, JSON.parse, data ) )
             }
 
             return deepExtend.apply( null, files )
@@ -107,51 +181,68 @@
                 strings = [ strings ]
             }
 
-            // allow 5 at a time
-            var queue = new Queue( this.opts.queueLength )
-
             var that = this
 
-            for ( var i = 0, l = strings.length; i < l; ++i )
-            {
-                var str = strings[ i ].toString()
+            mainQueue.add( function ( mainDone )
+                {
+                    var fileQueue = new Queue( that.opts.queueLength )
 
-                queue.add( function ( done )
+                    for ( var i = 0, l = strings.length; i < l; ++i )
                     {
-                        var filePath = path.join( that.opts.directory, str + '.json' )
+                        var str = strings[ i ].toString()
 
-                        if ( !that.opts.dynamic )
-                        {
-                            return done( null, readData.call( that, require, filePath ) )
-                        }
-
-                        fs.readFile( filePath, function ( err, data )
+                        fileQueue.add( function ( fileDone )
                             {
-                                if ( err )
+                                var filePath = path.join( that.opts.directory, str + '.json' )
+
+                                if ( !that.opts.dynamic )
                                 {
-                                    return done( err ) // error, file probably not found
+                                    return fileDone( null, parseData.call( that, require, filePath ) )
                                 }
 
-                                done( null, readData.call( that, JSON.parse, data ) )
+                                if ( fileData.hasOwnProperty( filePath ) )
+                                {
+                                    return fileDone( null, fileData[ filePath ] )
+                                }
+
+                                fs.readFile( filePath, function ( err, data )
+                                    {
+                                        if ( err )
+                                        {
+                                            return fileDone( err ) // error, file probably not found
+                                        }
+
+                                        var parsed = parseData.call( that, JSON.parse, data )
+
+                                        // reference cache and watch file
+                                        fileData[ filePath ] = parsed
+
+                                        watchFile.call( that, filePath )
+
+                                        fileDone( null, parsed )
+                                    }
+                                )
                             }
                         )
                     }
-                )
-            }
 
-            // finish up async queue
-            queue.done( function ( err, data )
-                {
-                    data = data || []
+                    // finish up async fileQueue
+                    fileQueue.done( function ( err, data )
+                        {
+                            data = data || []
 
-                    var combined = {}
+                            var combined = {}
 
-                    if ( 0 !== data.length )
-                    {
-                        combined = deepExtend.apply( null, data || [] )
-                    }
+                            if ( 0 !== data.length )
+                            {
+                                combined = deepExtend.apply( null, data || [] )
+                            }
 
-                    callback( err, combined )
+                            callback( err, combined )
+
+                            mainDone()
+                        }
+                    )
                 }
             )
         }
